5cdf0b77590798115777f39b62c074c57f081d0f
Amanbig <amanpreetsinghjhiwant@outlook.com>
2025-08-22 21:33:19 +0530

    feat: updated permissions and fixed min and max window error

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -2,6 +2,7 @@
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
+    
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
--- src/components/app/titleBar.tsx
+++ src/components/app/titleBar.tsx
@@ -2,18 +2,27 @@ import { Button } from "../ui/button";
 import { RxCross2 } from "react-icons/rx";
 import { VscChromeMinimize } from "react-icons/vsc";
 import { IoMdSquareOutline } from "react-icons/io";
+import { getCurrentWindow } from '@tauri-apps/api/window';
 
-export default function TitleBar(){
+const appWindow = getCurrentWindow();
+
+export default function TitleBar() {
     return (
         <div className="flex flex-row items-center justify-between pl-2 drag">
-        <div>
-            <p>FileEx</p>
-        </div>
-        <div className="no-drag">
-            <Button variant="ghost"><VscChromeMinimize /></Button>
-            <Button variant="ghost"><IoMdSquareOutline /></Button>
-            <Button variant="ghost"><RxCross2 /></Button>
-        </div>
+            <div>
+                <p>FileEx</p>
+            </div>
+            <div className="no-drag">
+                <Button variant="ghost" onClick={() => appWindow.minimize()}><VscChromeMinimize /></Button>
+                <Button variant="ghost" onClick={async () => {
+                    if (await appWindow.isMaximized()) {
+                        await appWindow.unmaximize();
+                    } else {
+                        await appWindow.maximize();
+                    }
+                }}><IoMdSquareOutline /></Button>
+                <Button variant="ghost" onClick={() => appWindow.close()}><RxCross2 /></Button>
+            </div>
         </div>
     );
 }
